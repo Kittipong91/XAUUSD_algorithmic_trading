a6274c1222934d3105f008f9478adaad5a7464b9
Kittipong <kittipongpon91@gmail.com>
2023-12-11 22:02:01 +0700

    update code

--- config/constants.py
+++ config/constants.py
@@ -2,6 +2,10 @@ PATH_DATA = '../../data/'
 
 SYMBOL = {
     'XAUUSD' : {
+        'M1': 'XAUUSD/XAUUSD_M1 - XAUUSD_M1',
+        'M5': 'XAUUSD/XAUUSD_M5 - XAUUSD_M5',
+        'M15': 'XAUUSD/XAUUSD_M15 - XAUUSD_M15',
+        'M30': 'XAUUSD/XAUUSD_M30 - XAUUSD_M30',
         'H1': 'XAUUSD/XAUUSD_H1 - XAUUSD_H1',
         'H4' : 'XAUUSD/XAUUSD_H4 - XAUUSD_H4',
         'D1': 'XAUUSD/XAUUSD_D1 - XAUUSD_D1',
