660c9705c3fed8626d15e4987b01f387d73951ce
Kittipong <kittipongpon91@gmail.com>
2024-01-05 02:43:20 +0700

    Linear ML

--- util/plot_equity.py
+++ util/plot_equity.py
@@ -1,12 +1,9 @@
 import matplotlib.pyplot as plt
 
 
-def plot_equity_trades(df,label,account_size) :
+def plot_equity_trades(df, label, account_size):
     df = df.copy()
-    df['cum_res'] = df['PnL'].cumsum() + account_size
-
-    plt.figure(figsize=(26, 10))
-    plt.plot(df['ExitTime'],df['cum_res'], label= label)
+    plt.plot(df.index, df['Equity'], label=label)
     plt.legend()
     plt.title('Equity', fontsize=18)
 
