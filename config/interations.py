40369fc69f88b19985d992c7be4dc75663a55003
Kittipong <kittipongpon91@gmail.com>
2024-01-08 23:15:31 +0700

    Reports backtest of Technical Strategy

---
+++ config/interations.py
@@ -0,0 +1,49 @@
+from datetime import datetime
+iterations = [
+    {
+        'in_sample': [datetime(2007, 1, 1), datetime(2010, 12, 31)],
+        'out_of_sample': [datetime(2011, 1, 1), datetime(2012, 12, 31)]
+    },
+    {
+        'in_sample': [datetime(2008, 1, 1), datetime(2011, 12, 31)],
+        'out_of_sample': [datetime(2012, 1, 1), datetime(2013, 12, 31)]
+    },
+    {
+        'in_sample': [datetime(2009, 1, 1), datetime(2012, 12, 31)],
+        'out_of_sample': [datetime(2013, 1, 1), datetime(2014, 12, 31)]
+    },
+    {
+        'in_sample': [datetime(2010, 1, 1), datetime(2013, 12, 31)],
+        'out_of_sample': [datetime(2014, 1, 1), datetime(2015, 12, 31)]
+    },
+    {
+        'in_sample': [datetime(2011, 1, 1), datetime(2014, 12, 31)],
+        'out_of_sample': [datetime(2015, 1, 1), datetime(2016, 12, 31)]
+    },
+    {
+        'in_sample': [datetime(2012, 1, 1), datetime(2015, 12, 31)],
+        'out_of_sample': [datetime(2016, 1, 1), datetime(2017, 12, 31)]
+    },
+    {
+        'in_sample': [datetime(2013, 1, 1), datetime(2016, 12, 31)],
+        'out_of_sample': [datetime(2017, 1, 1), datetime(2018, 12, 31)]
+    },
+    {
+        'in_sample': [datetime(2014, 1, 1), datetime(2017, 12, 31)],
+        'out_of_sample': [datetime(2018, 1, 1), datetime(2019, 12, 31)]
+    },
+    {
+        'in_sample': [datetime(2015, 1, 1), datetime(2018, 12, 31)],
+        'out_of_sample': [datetime(2019, 1, 1), datetime(2020, 12, 31)]
+    },
+    {
+        'in_sample': [datetime(2016, 1, 1), datetime(2019, 12, 31)],
+        'out_of_sample': [datetime(2020, 1, 1), datetime(2021, 12, 31)]
+    },
+    {
+        'in_sample': [datetime(2017, 1, 1), datetime(2020, 12, 31)],
+        'out_of_sample': [datetime(2021, 1, 1), datetime(2022, 12, 31)]
+    }
+]
+
+# 3:1
