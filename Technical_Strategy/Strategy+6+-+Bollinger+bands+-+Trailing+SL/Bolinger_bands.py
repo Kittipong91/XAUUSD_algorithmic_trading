d31570748f9cb6b66c41b25adbd23732f32abc69
Kittipong <kittipongpon91@gmail.com>
2024-01-09 02:56:51 +0700

    reports ML

--- Technical_Strategy/Strategy+6+-+Bollinger+bands+-+Trailing+SL/Bolinger_bands.py
+++ Technical_Strategy/Strategy+6+-+Bollinger+bands+-+Trailing+SL/Bolinger_bands.py
@@ -6,13 +6,17 @@ from util.Bollinger_bands import *
 
 class Bolinger_bands(Strategy):
     ATR_SL = 0.5
+    Mutiple_TP = 10
+
+    band_Low =3
+    band_High = 50
 
     def init(self):
         self.data.df['ATR'] = ATR(self.data.df, 20)['ATR']
         self.data.df['sma_fast'] = SMA(self.data.df, 50, 200)['sma_fast']
 
         self.data.df['upper_band'] = Bollinger_bands(
-            self.data.df, 50, 3)['upper_band']
+            self.data.df, self.band_High, self.band_Low)['upper_band']
 
     def next(self):
 
@@ -21,13 +25,13 @@ class Bolinger_bands(Strategy):
         current_ATR = self.data.df.loc[self.data.df.index[-1], 'ATR']
 
         if not self.position and sma_fast >= sma_low and any(self.data.df['Close'][-3: -1].values) <= any(self.data.df['lower_band'][-3: -1].values) and self.data.df['Close'][-1] >= self.data.df['rolling_mean'][-1]:
-            tp = self.data.Close + current_ATR * self.ATR_SL * 10
+            tp = self.data.Close + current_ATR * self.ATR_SL * self.Mutiple_TP
             sl = self.data.Close - current_ATR * self.ATR_SL
             # Place your buy logic here
             self.buy(sl=sl, tp=tp, size=1)
 
         if not self.position and sma_fast <= sma_low and any(self.data.df['Close'][-3: -1].values) >= any(self.data.df['lower_band'][-3: -1].values) and self.data.df['Close'][-1] <= self.data.df['rolling_mean'][-1]:
-            tp = self.data.Close - current_ATR * self.ATR_SL * 10
+            tp = self.data.Close - current_ATR * self.ATR_SL * self.Mutiple_TP
             sl = self.data.Close + current_ATR * self.ATR_SL
 
             self.sell(sl=sl, tp=tp, size=1)
