5d179c3defbc3efdbeb537774132f01364721424
Kittipong <kittipongpon91@gmail.com>
2024-01-25 20:08:42 +0700

    fix reports

--- Live_Trading/strategies/Momentum.py
+++ Live_Trading/strategies/Momentum.py
@@ -53,7 +53,7 @@ strategy = 'Momentum'
 
 def check_time() :
     current_time = time.strftime("%H:%M:%S")
-    print(current_time, 'waiting......')
+    # print(current_time, 'waiting......')
 
 def run_momentum():
     while True:
